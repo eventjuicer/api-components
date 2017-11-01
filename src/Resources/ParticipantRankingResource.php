bc7efcc473efca2d2fa359e6b4485f1e8908a458
Adam Zygadlewicz <adam@zygadlewicz.com>
2017-11-01 14:47:57 +0100

    update

---
+++ src/Resources/ParticipantRankingResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Eventjuicer\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+//use Eventjuicer\Models\Participant;
+
+use Eventjuicer\ValueObjects\EmailAddress;
+
+
+
+class ParticipantRankingResource extends Resource
+{
+
+    protected $presenterFields = ["fname", "lname", "cname2", "phone"];
+
+
+    public function toArray($request)
+    {
+
+		//$data = parent::toArray($request);
+
+		$data = $this->fields->whereIn("name", $this->presenterFields)->mapWithKeys(function($item)
+        {     
+            return [ $item->name => $item->pivot->field_value] ;
+
+        })->all();
+
+        $data["id"] = $this->id;
+
+        $data["email"] = (new EmailAddress($this->email))->obfuscated();
+
+        $data["phone"] = str_pad(substr($data["phone"], -4), 9, "x", STR_PAD_LEFT);
+
+		return $data;
+            
+    }
+}
+
+
+
