d3ac0437605bac7546a9998c0a7762b36fa84821
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-03-28 15:05:37 +0200

    merge profile with defaults

--- src/Resources/PublicVisitor.php
+++ src/Resources/PublicVisitor.php
@@ -12,18 +12,21 @@ use Eventjuicer\Services\Hashids;
 class PublicVisitor extends Resource
 {
 
-    protected $presenterFields = ["fname", "lname", "cname2", "position, phone"];
+    protected $presenterFields = ["fname", "lname", "cname2", "position"];
 
 
     public function toArray($request)
     {
 
 
-            $data = $this->fields->whereIn("name", $this->presenterFields)->mapWithKeys(function($item)
+            $profile = $this->fields->whereIn("name", $this->presenterFields)->mapWithKeys(function($item)
             {     
                 return [ $item->name => $item->pivot->field_value ] ;
 
             })->all();
+
+            $data = array_merge(array_fill_keys($this->presenterFields, ""), $profile);
+
 
             $data["id"] = (int) $this->id;
             $data["ns"] = "participant";
