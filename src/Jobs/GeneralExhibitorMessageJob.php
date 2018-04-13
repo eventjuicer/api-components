7b6a9ad4d3beeaa62443debecf0661cf644a086c
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-04-13 15:29:11 +0200

    general message

---
+++ src/Jobs/GeneralExhibitorMessageJob.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Eventjuicer\Jobs;
+
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\GeneralExhibitorEmail as Email;
+
+
+use Eventjuicer\Models\Participant;
+use Eventjuicer\Repositories\ParticipantDeliveryRepository;
+use Eventjuicer\Services\Revivers\ParticipantSendable;
+ 
+
+class GeneralExhibitorMessageJob extends Job // implements ShouldQueue
+{
+    
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    protected $participant, $eventId;
+    public $subject, $view;
+
+    public function __construct(Participant $participant, int $eventId, array $config)
+    {
+        
+        $this->participant = $participant;
+        $this->eventId = $eventId;
+
+        $this->view = array_get($config, "email");
+        $this->subject = array_get($config, "subject", "Organizacyjnie...");
+
+    }
+
+    public function handle(
+        ParticipantDeliveryRepository $deliveries, 
+        ParticipantSendable $sendable)
+    {
+
+
+        //do we have company assigned?
+
+        if(!$this->participant->company_id)
+        {
+            return;
+        }
+
+        // double check !
+
+        if(! $sendable->filter( collect([])->push( $this->participant ), $this->eventId)->count() )
+        {
+            return;
+        }
+
+
+        Mail::send(
+            new Email( $this->participant, $this->subject, $this->view )
+        );
+
+
+        if(! env("MAIL_TEST", true))
+        {
+            $deliveries->updateAfterSend( $this->participant->email, $this->eventId );
+        }
+
+    }
+
+}
