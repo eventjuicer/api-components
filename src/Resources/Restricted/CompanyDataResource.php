00cedbb9fa6b72edb35553c670d77410b5b81eac
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-01-10 22:54:25 +0100

    rename resource

--- src/Resources/Restricted/CompanyDataResource.php
+++ src/Resources/Restricted/CompanyDataResource.php
@@ -25,7 +25,7 @@ class CompanyDataResource extends Resource
             $data["id"]     = $this->id;
             $data["name"]   = $this->name;
             $data["value"]  = $this->value;
-            $data["excerpt"]  = is_string($this->value) ? mb_substr(strip_tags($this->value), 0, 50) : $this->value;
+            $data["summary"]  = is_string($this->value) ? mb_substr(strip_tags($this->value), 0, 50) : $this->value;
             $data["created_at"] = (string) $this->created_at;
             $data["updated_at"] = (string) $this->updated_at;
 
