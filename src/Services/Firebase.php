bff5e19a93ebdec44b986ba4127738da515fb54c
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-02-19 17:59:38 +0100

    fix

--- src/Services/Firebase.php
+++ src/Services/Firebase.php
@@ -20,8 +20,6 @@ class Firebase {
 
 		$this->config = json_decode(fixLocalFileJson(file_get_contents(app()->basePath(".firebase"))));
 
-		return;
-
 		if(is_null($this->config))
 		{
 			throw new \Exception("Error when reading firebase config. " . json_last_error_msg());
