057f6a4559c28f212690c66ec6eeb6f565d46517
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-01-09 02:20:12 +0200

    admin email helper class

---
+++ src/Services/Exhibitors/Email.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Eventjuicer\Services\Exhibitors;
+
+use Eventjuicer\Models\Participant as Model;
+
+class Email {
+	
+	protected $model;
+
+	function __construct(Model $model){
+		
+		$this->model = $model;
+	}
+
+	function getAdmin(){
+		
+		if($this->model->company->admin_id){
+			return $this->model->company->admin->all();
+		}
+
+		return $this->model->organizer->users->where("is_default", 1)->first()->all();
+
+		//get default admin for an organizer!
+
+	}
+
+	function getSender(){
+
+		$admin = $this->getAdmin();
+
+		return $admin->fname . " " . $admin->lname;
+	}
+
+	function getFooter(){
+		
+		$admin = $this->getAdmin();
+
+		return "
+
+" . array_get($admin, "fname") . " " . array_get($admin, "lname") . "
+
+" . array_get($admin, "position") . "
+
+" . array_get($admin, "email") . " " . array_get($admin, "phone") . "
+
+";
+	}
+
+}
