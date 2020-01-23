f2445c9dc3b6a1305c2152e7220f612095fce4f0
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-01-23 18:25:58 +0100

    output resized image

--- src/Resources/PublicPartnerPerformanceResource.php
+++ src/Resources/PublicPartnerPerformanceResource.php
@@ -4,7 +4,8 @@ namespace Eventjuicer\Resources;
 
 use Illuminate\Http\Resources\Json\Resource;
 use Eventjuicer\Services\Exhibitors\CompanyData;
- 
+use Eventjuicer\ValueObjects\CloudinaryImage;
+
 class PublicPartnerPerformanceResource extends Resource
 {
 
@@ -19,7 +20,7 @@ class PublicPartnerPerformanceResource extends Resource
 
 		$data["company_id"] = $this->company_id;
         $data["name"] = $cd->getName() ?? $this->company->slug;
-        $data["logotype"] = $cd->getLogotypeCdn();
+        $data["logotype"] = (new CloudinaryImage($cd->getLogotypeCdn()))->thumb();
 	    $data["stats"] = isset($this->company->stats) ? $this->company->stats : [];
 
 		return $data;
