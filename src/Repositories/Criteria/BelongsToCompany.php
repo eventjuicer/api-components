a764b00333590e2324fa03531ffed7ebfbb0fff0
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-02-08 22:59:44 +0100

    fix

--- src/Repositories/Criteria/BelongsToCompany.php
+++ src/Repositories/Criteria/BelongsToCompany.php
@@ -17,7 +17,7 @@ class BelongsToCompany extends Criteria {
 
             $company = Company::where("slug", "like", $id)->where("organizer_id", $organizer_id)->get();
 
-            $this->id = $company->first()->id;
+            $this->id = $company->first() ? $company->first()->id : 0;
 
         }else{
             $this->id = (int) $id;
