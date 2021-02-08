c49bda88415fb7e0050ddde110533e59dbc900ba
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-02-08 21:21:45 +0100

    handle company slug

--- src/Repositories/Criteria/BelongsToCompany.php
+++ src/Repositories/Criteria/BelongsToCompany.php
@@ -5,13 +5,24 @@ namespace Eventjuicer\Repositories\Criteria;
 use Bosnadev\Repositories\Criteria\Criteria;
 use Bosnadev\Repositories\Contracts\RepositoryInterface as Repository;
 
+use Eventjuicer\Models\Company;
+
 class BelongsToCompany extends Criteria {
 
     protected $id;
 
-    function __construct($id)
-    {
-        $this->id = (int) $id;
+    function __construct($id, int $organizer_id = 0){
+
+        if( $organizer_id > 0 && !is_numeric($id) ){
+
+            $company = Company::where("slug", "like", $id)->where("organizer_id", $organizer_id)->get();
+
+            $this->id = $company ? $company->first() : 0;
+
+        }else{
+            $this->id = (int) $id;
+        }
+
     }
 
     /**
