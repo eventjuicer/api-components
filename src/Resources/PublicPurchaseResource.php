5849c674a1ede0f8e58913cf601fb046a0fbe583
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-02-09 15:54:32 +0100

    https://github.com/ecommerceberlin/targiehandlu.pl/issues/106

---
+++ src/Resources/PublicPurchaseResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Eventjuicer\Resources;
+
+use Illuminate\Http\Resources\Json\Resource; 
+use Eventjuicer\ValueObjects\EmailAddress;
+
+class PublicPurchaseResource extends Resource {
+
+    public function toArray($request){   
+
+        $data = array();
+
+        $data["id"] = $this->id;
+        $data["domain"] = (new EmailAddress($this->email))->domain();
+        $data["slug"] = $this->company ? $this->company->slug: null;
+        $data["created_at"] = (string) $this->createdon;
+
+        return $data;
+    }
+
+
+}
+
+
+
