a4da21c0df347c851787597cb3ab6eeffc61fdb5
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-07 16:50:13 +0100

    strip tags only for TEH

--- src/Resources/PublicCompanyResource.php
+++ src/Resources/PublicCompanyResource.php
@@ -80,7 +80,7 @@ class PublicCompanyResource extends Resource
 
         $profile = array_merge(array_flip(self::$presenterFields), $this->data->whereIn("name", self::$presenterFields)->mapWithKeys(function($item){     
 
-                    return [$item->name => is_string($item->value)? strip_tags($item->value): $item->value];
+                    return [$item->name => intval($this->group_id)===1 && is_string($item->value)? strip_tags($item->value): $item->value];
 
         })->all());
 
