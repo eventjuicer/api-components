8830f24b7d1248f8fcccf5aa3caa2ab732801305
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-08 21:49:45 +0100

    fix

--- src/Resources/PublicCompanyResource.php
+++ src/Resources/PublicCompanyResource.php
@@ -75,11 +75,13 @@ class PublicCompanyResource extends Resource
 
     public function toArray($request){   
 
+        // dd($this->resource);
+
         $defaultLang = $this->group_id > 1 ? "en" : "pl";
 
         $profile = array_merge(array_flip(self::$presenterFields), $this->data->whereIn("name", self::$presenterFields)->mapWithKeys(function($item) {     
 
-                    return [$item->name => is_string($item->value)? strip_tags($item->value): $item->value];
+                    return [$item->name =>  (int)$this->group_id === 1 & is_string($item->value)? strip_tags($item->value): $item->value];
 
         })->all());
 
