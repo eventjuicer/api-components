6a5cabff864b941705184c4b03c3a2892979024e
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-03 20:04:49 +0200

    fetch visitor by email

---
+++ src/Crud/Participants/Fetch.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Eventjuicer\Crud\Participants;
+
+use Eventjuicer\Crud\Crud;
+use Eventjuicer\Repositories\ParticipantRepository;
+use Eventjuicer\Repositories\Criteria\BelongsToEvent;
+use Eventjuicer\Repositories\Criteria\ColumnMatches;
+use Eventjuicer\Repositories\Criteria\SortBy;
+use Eventjuicer\Repositories\Criteria\WhereHas;
+
+class Fetch extends Crud  {
+
+    protected $repo;
+
+    function __construct(ParticipantRepository $repo){
+        $this->repo = $repo;
+    }
+
+ 
+    public function getVisitorsByEmail($email = ""){
+
+        $event_id = $this->getContextFromHost()->getEventId();
+
+        if(!$event_id || !$email){
+            return collect([]);
+        }
+
+        $this->repo->pushCriteria(new BelongsToEvent(  $event_id ));
+        $this->repo->pushCriteria(new ColumnMatches("email", strtolower( trim($email)) ));
+        $this->repo->pushCriteria(new WhereHas("purchases.tickets", function($q){
+            $q->where("role", "visitor");
+        }));
+        $this->repo->pushCriteria(new SortBy("id", "DESC"));
+        $this->repo->with(["fields","purchases.tickets"]);
+        $res = $this->repo->all();
+       return $res;
+    }
+
+
+}
+
+
