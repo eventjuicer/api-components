363bea64e14327fe7361fedc7d3b9ece11133b9e
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-10-20 13:50:52 +0200

    Update MeetupResource.php

--- src/Resources/Restricted/MeetupResource.php
+++ src/Resources/Restricted/MeetupResource.php
@@ -38,7 +38,7 @@ class MeetupResource extends Resource
 
         $data["direction"] = (string) $this->direction;
 
-        if($this->agreed || $this->direction == "LTD" ){
+        if($this->agreed){
             $data["participant"] = new FullVisitorResource($this->participant);
         }else{
             $data["participant"] = new VisitorResource($this->participant);
