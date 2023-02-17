941a3238c310e606524c5d52fc4b2265c2b589dc
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-02-17 10:20:14 +0100

    Update AdminParticipantExportResource.php

--- src/Resources/AdminParticipantExportResource.php
+++ src/Resources/AdminParticipantExportResource.php
@@ -11,24 +11,28 @@ class AdminParticipantExportResource extends Resource
     public function toArray($request)
     {
 
-        
-
 
         return [
-
 
 
             "id" => $this->id,
             
             "email" => $this->email,
 
-            "going" => $this->ticketdownload ? (int) $this->ticketdownload->going: null,
+            "going" => !is_null($this->ticketdownload) ? (int) $this->ticketdownload->going: "N/A",
 
             "code" => (new Hashids)->encode($this->id),
 
-            "ticket_ids" => $this->ticketpivot->filter(function($item){
-                return $item->sold;
-            })->pluck("ticket_id")->all(),
+            "important" => (int) $this->important,
+
+            "profile" => $this->fields->mapWithKeys(function($_item){
+                
+                return [$_item->name => $_item->pivot->field_value];
+            }),
+
+            // "ticket_ids" => $this->ticketpivot->filter(function($item){
+            //     return $item->sold;
+            // })->pluck("ticket_id")->all(),
 
             "created_at" => $this->createdon
 
@@ -39,6 +43,4 @@ class AdminParticipantExportResource extends Resource
 
     }
 }
-
-
 
