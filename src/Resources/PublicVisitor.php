3be92c22845daa12acd53d743abed16f0e1c9217
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-02-19 23:13:28 +0100

    show more info

--- src/Resources/PublicVisitor.php
+++ src/Resources/PublicVisitor.php
@@ -19,8 +19,9 @@ class PublicVisitor extends Resource
 
             $data["id"] = (int) $this->id;
             $data["fname"] = (string) $profile->fname;
+            $data["lname"] = $this->stringToSecret($profile->lname);
             $data["cname2"] = (string) $profile->cname2;
-            $data["phone"] = str_pad ( substr( (string) $profile->phone, -3) , 9, "*", STR_PAD_LEFT);
+            $data["phone"] = str_pad ( substr( (string) $profile->phone, 0, 3) , 9, "*", STR_PAD_RIGHT);
             $data["vip"] = (string) $profile->isVip();
             $data["ns"] = "participant";
             $data["email"] = (new EmailAddress($this->email))->obfuscated();
@@ -28,6 +29,18 @@ class PublicVisitor extends Resource
 
            return $data;
     }
+
+    private function stringToSecret( $string = NULL)
+    {
+        if (!$string) {
+            return NULL;
+        }
+        $length = mb_strlen($string);
+        $visibleCount = (int) round($length / 4);
+        $hiddenCount = $length - ($visibleCount * 2);
+        return mb_substr($string, 0, $visibleCount) . str_repeat('*', $hiddenCount) . mb_substr($string, ($visibleCount * -1), $visibleCount);
+    }
+
 }
 
 
