e8c8c733b7f9419daef27a20e7b64bc0e7819ef1
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-02-20 23:21:29 +0100

    search by code

--- src/Services/Search/ParticipantSearch.php
+++ src/Services/Search/ParticipantSearch.php
@@ -7,6 +7,7 @@ use  Bosnadev\Repositories\Eloquent\Repository;
 use Eventjuicer\Repositories\Criteria\BelongsToEvent;
 use Eventjuicer\Repositories\Criteria\ColumnMatches;
 use Eventjuicer\Models\ParticipantFields;
+use Eventjuicer\Models\Participant;
 
 class ParticipantSearch {
 
@@ -16,11 +17,13 @@ protected $result;
 
 function __construct(Repository $repo, int $event_id, string $q, $addFields = []){
 
+        $q = trim($q);
+
         if(!$q || strlen($q) < 4){
             return;
         }
 
-        if(strpos($q, "@")!==false){
+        if( strpos($q, "@")!==false ){
 
             $repo->with(["fields"]);
             $repo->pushCriteria(new BelongsToEvent($event_id));
@@ -28,31 +31,36 @@ function __construct(Repository $repo, int $event_id, string $q, $addFields = []
                 
             $this->result = $repo->all();
         
-        }else{
+        }
 
-            if(is_numeric($q)){
+        else if( is_numeric($q) ){
 
-                $rows = ParticipantFields::with("participant", "participant.fields")->where("event_id", $event_id)->where("field_id", 8)->where("field_value", "LIKE", $q."%")->get()->pluck("participant");  
+            $rows = ParticipantFields::with("participant", "participant.fields")->where("event_id", $event_id)->where("field_id", 8)->where("field_value", "LIKE", $q."%")->get()->pluck("participant");  
 
-                $this->result = $rows;   
+            $this->result = $rows;   
 
-           }else{
+        }else if( is_numeric(ltrim($q, "cC")) &&  ltrim($q, "cC") > 152090 ){
 
-                $rows1 = ParticipantFields::with("participant", "participant.fields")->where("event_id", $event_id)->whereIn("field_id", array_merge([3,11], (array) $addFields))->where("field_value", "LIKE", $q."%")->get()->pluck("participant");
-                $rows2 = collect([]);
+            $lookup = Participant::find( ltrim($q, "cC") );
 
-                if(strlen($q)>4){
-                    $repo->with(["fields"]);
-                    $repo->pushCriteria(new BelongsToEvent($event_id));
-                    $repo->pushCriteria(new ColumnMatches("email", "%" . $q . "%"));    
-                    $rows2 = $repo->all();
-                }
+            $this->result =  $lookup && $lookup->event_id==$event_id ? collect([ $lookup ]): collect([]);
+        }
+        else{
+  
+                $rows = ParticipantFields::with("participant", "participant.fields")->where("event_id", $event_id)->whereIn("field_id", array_merge([3,11], (array) $addFields))->where("field_value", "LIKE", $q."%")->get()->pluck("participant");
+                //$rows2 = collect([]);
 
-                $rows = $rows1->merge( $rows2  );
+                // if(strlen($q)>4){
+                //     $repo->with(["fields"]);
+                //     $repo->pushCriteria(new BelongsToEvent($event_id));
+                //     $repo->pushCriteria(new ColumnMatches("email", "%" . $q . "%"));    
+                //     $rows2 = $repo->all();
+                // }
+
+                //$rows = $rows1->merge( $rows2  );
             
-                $this->result = $rows->unique("id")->values();
+                $this->result = $rows;//->unique("id")->values();
 
-            }
         }
 }
 
