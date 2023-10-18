77b61410473dafd5b8f98ec5d50fe457bbe25b02
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-10-18 23:17:25 +0200

    reset to original

--- src/Repositories/CompanyRepository.php
+++ src/Repositories/CompanyRepository.php
@@ -7,7 +7,6 @@ use Eventjuicer\Repositories\Repository;
 
 use Carbon\Carbon;
 use Closure;
-use Log;
 
 class CompanyRepository extends Repository
 {
@@ -20,60 +19,45 @@ class CompanyRepository extends Repository
     }
 
 
-	/** it should return Company  */
-	
     public function updateStatsIfNeeded($id, Closure $source)
     {
-
 
     	$company = $this->find($id);
 
     	if(!$company) 
     	{
-    		throw new \Exception("Company $id missing...");
+    		return [];
     	}
 
     	if(! is_null($company->stats_updated_at) && Carbon::now()->diffInMinutes( $company->stats_updated_at ) < 15){
     		
 			//FRESH -> get from database!
-//			return $company->only( ["position", "points"] )->all();
+			return $company->only( ["position", "points"] );
     	}
 
-		/**
-		 * calling ->getExhibitorRankingPosition
-		 * Collection of Participants
-		 */
-
-    	$exhibitorsWithStats = $source($id);
+    	$exhibitorsWithStats = $source();
 
 		/** Collection of participants */
 
 		$exhibitor = $exhibitorsWithStats->where("company_id", $id)->first();
 
 		if(!$exhibitor){
-			 return [];
+			return $company;
 		}
 
 		$points =  array_get($exhibitor->company->stats, "sessions", 0);
 		$position = array_get($exhibitor->company->stats, "position", 0);
 
+		$this->update([
+			"stats_updated_at" => Carbon::now(),
+			"points" => $points,
+			"position" => $position
+		], $id);
 
-		if($points>0){
-
-			$this->update([
-				"stats_updated_at" => Carbon::now(),
-				"points" => $points,
-				"position" => $position
-			], $id);
-		}
-
-		
-
-		return compact("points", "position");   
+		return $this->find($id);   
 
     }
 
 
 
 }
-
