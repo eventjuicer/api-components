ec89e9436af68829ca2d330b39b1663a303d9ab5
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-02-23 23:02:34 +0100

    Update OnlyExhibitorRoleTransform.php

--- src/Crud/Companies/OnlyExhibitorRoleTransform.php
+++ src/Crud/Companies/OnlyExhibitorRoleTransform.php
@@ -9,6 +9,7 @@ class OnlyExhibitorRoleTransform {
 
     public $repo;
     
+    public $allowedRoles = ["exhibitor", "presenter"];
 
     function __construct(CompanyRepository $repo){
         $this->repo = $repo;
@@ -16,13 +17,17 @@ class OnlyExhibitorRoleTransform {
 
     public function transform(Company $item){
 
-        $item->participants = $item->participants->filter(function($registration) {
 
-            $role = $registration->ticketpivot->pluck("ticket.role");
+        $item->participants->transform(function($participant){
 
-            return $role->contains("exhibitor") || $role->contains("presenter");
+            $has_allowed_roles = $participant->ticketpivot->filter(function($ticketpivot){
 
+                return in_array( $ticketpivot->ticket->role, $this->allowedRoles);
+    
+            });
 
+            return $has_allowed_roles->count()? $participant : null;
+            
         });
         
         return $item;
