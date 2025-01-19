1702559736db6655c700b13bc32b6ef785e91b28
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-01-19 23:49:06 +0100

    Update ParticipantResource.php

--- src/Resources/ExtApi/ParticipantResource.php
+++ src/Resources/ExtApi/ParticipantResource.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Resources\Json\Resource;
 use Eventjuicer\Services\Hashids;
 
 
+
+
 class ParticipantResource extends Resource
 {
+
+    public static $companyFields = ["name", "keywords", "logotype_cdn"];
+
 
     public function toArray($request)
     {
@@ -17,15 +22,21 @@ class ParticipantResource extends Resource
         $data["fields"] = $this->fields->mapWithKeys(function($item)
         {     
             return [ $item->name => $item->pivot->field_value] ;
-
         })->all();
 
-        
+        $data["fields"]["company"] =  $this->company? $this->company->data->whereIn("name", static::$companyFields)->mapWithKeys(function($item){
+            return [ $item->name => $item->value ] ;
+        }): [];
+
+        if($this->company && $this->company->id){
+            $data["fields"]["company"]["promo"] = $this->company->promo;
+        }
+
         $data["roles"] = $this->purchases->filter(function($item){
-
             return $item->status != "cancelled" ;
-
         })->pluck("tickets")->collapse()->pluck("role")->all();
+
+       
 
     
         $data["code"] = (new Hashids())->encode( $this->id );
