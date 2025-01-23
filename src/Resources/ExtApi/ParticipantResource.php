ef1592e8308e434ea0976c27d57301d83aaccb57
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-01-23 19:38:23 +0100

    handle vip better

--- src/Resources/ExtApi/ParticipantResource.php
+++ src/Resources/ExtApi/ParticipantResource.php
@@ -36,8 +36,7 @@ class ParticipantResource extends Resource
             return $item->status != "cancelled" ;
         })->pluck("tickets")->collapse()->pluck("role")->all();
 
-       
-
+        $data["important"] = $this->important || !empty($data["fields"]["important"]);
     
         $data["code"] = (new Hashids())->encode( $this->id );
 
