afe2fbaa15e08b6fdd7e956adbd1ca3f41dbed58
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-10-16 22:38:21 +0200

    Update PublicMeetupResource.php

--- src/Resources/PublicMeetupResource.php
+++ src/Resources/PublicMeetupResource.php
@@ -11,16 +11,16 @@ class PublicMeetupResource extends Resource {
     
         $data = [];
 
-        $data["id"] = $this->id;
+
         // $data["direction"] = (string) $this->direction;
-        $data["company_id"] = $this->company_id;
-        $data["applicant"] = $this->participant_id;
-        $data["trainer"] = $this->rel_participant_id;
-        $data["agreed"] =  (int) $this->agreed;
+        $data["t_c_id"] = $this->company_id;
+        $data["a"] = $this->participant_id;
+        $data["t_id"] = $this->rel_participant_id;
+        $data["ok"] =  (int) $this->agreed;
         // $data["retries"] = (int) $this->retries;
         // $data["sent_at"] = (string) $this->sent_at;
         // $data["resent_at"] = (string) $this->resent_at;
-        $data["responded_at"] = (string) $this->responded_at;
+        $data["t_at"] = (string) $this->responded_at;
         // $data["scheduled_at"] = (string) $this->scheduled_at;
         // $data["created_at"] = (string) $this->created_at;
         // $data["updated_at"] = (string) $this->updated_at;
