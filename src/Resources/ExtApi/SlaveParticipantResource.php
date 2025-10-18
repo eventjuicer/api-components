8c3ba464bbb5f48d440e1c03c33283b35b201dc9
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-10-19 00:02:05 +0200

    Update SlaveParticipantResource.php

--- src/Resources/ExtApi/SlaveParticipantResource.php
+++ src/Resources/ExtApi/SlaveParticipantResource.php
@@ -10,15 +10,24 @@ class SlaveParticipantResource extends Resource
 
     public function toArray($request){
         
-        $data = parent::toArray($request);
+        $data = [];
+
+        $data["id"] = $this->id;
+        $data["email"] = $this->email;
+        $data["token"] = $this->token;
+        $data["parent_id"] = $this->parent_id;
+
+        $data["important"] = intval($this->important || !empty($data["fields"]["important"]) );
+        
+        $data["code"] = (new Hashids())->encode( $this->id );
+
+        $data["created_at"] = $this->createdon;
 
         $data["fields"] = $this->fields->mapWithKeys(function($item)
         {     
             return [ $item->name => $item->pivot->field_value] ;
         })->all();
 
-        $data["important"] = intval($this->important || !empty($data["fields"]["important"]) );
-        $data["code"] = (new Hashids())->encode( $this->id );
 
         return $data;
 
